c600c500ad330c9b1629a4a134a10a08b6b57f17
pilif139 <pifilif@gmail.com>
2025-01-22 03:01:20 +0100

    fixed database connection errors

--- Database/Database.cs
+++ Database/Database.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DotNetEnv;
+using Microsoft.EntityFrameworkCore;
 using MySqlConnector;
 using projekt_po.Model;
 using projekt_po.Utils;
@@ -11,11 +12,12 @@ public class DatabaseContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        string? server = Environment.GetEnvironmentVariable("DB_SERVER");
-        string? port = Environment.GetEnvironmentVariable("DB_PORT");
-        string? database = Environment.GetEnvironmentVariable("DB_NAME");
-        string? user = Environment.GetEnvironmentVariable("DB_USER");
-        string? password = Environment.GetEnvironmentVariable("DB_PASSWORD");
+        Env.Load();
+        var server = Environment.GetEnvironmentVariable("DB_SERVER");
+        var port = Environment.GetEnvironmentVariable("DB_PORT");
+        var database = Environment.GetEnvironmentVariable("DB_NAME");
+        var user = Environment.GetEnvironmentVariable("DB_USER");
+        var password = Environment.GetEnvironmentVariable("DB_PASSWORD");
         if (StringUtils.AreStringsNullOrEmpty(server, port, database, user, password))
         {
             throw new Exception("You need to define database variables in .env file");
@@ -30,10 +32,13 @@ public class DatabaseContext : DbContext
         catch (ArgumentException e)
         {
             Console.WriteLine("Invalid connection string: " + e.Message);
+            Environment.Exit(1);
+            
         }
         catch (MySqlException e)
         {
             Console.WriteLine("Error connecting to database: " + e.Message);
+            Environment.Exit(1);
         }
 
     }
--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 ﻿## Projekt z programowania obiektowego w c#
 
+Before running program you should create an .env file with variables:
+- `DB_SERVER`
+- `DB_PORT`
+- `DB_USER`
+- `DB_PASSWORD`
+- `DB_NAME`
+
 ## sources:
  - https://learn.microsoft.com/en-us/ef/core/
  - https://learn.microsoft.com/en-us/ef/core/managing-schemas/migrations/?tabs=dotnet-core-cli
