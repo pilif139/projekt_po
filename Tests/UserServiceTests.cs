3324844a7122ad84a2419ba9d9e151f07090c611
pilif139 <pifilif@gmail.com>
2025-01-22 22:21:18 +0100

    added more tests and Dispose function that checks if loggerMock was called after every test

--- Tests/UserServiceTests.cs
+++ Tests/UserServiceTests.cs
@@ -6,7 +6,7 @@ using projekt_po.Services;
 
 namespace Tests;
 
-public class UserServiceTests
+public class UserServiceTests : IDisposable
 {
     private readonly Mock<IUserRepository> _userRepositoryMock;
     private readonly Mock<ILogger> _loggerMock;
@@ -18,7 +18,7 @@ public class UserServiceTests
         _loggerMock = new Mock<ILogger>();
         _userService = new UserService(_userRepositoryMock.Object, _loggerMock.Object);
     }
-    
+
     [Fact]
     public void AddUser_ShouldAddUser()
     {
@@ -27,12 +27,65 @@ public class UserServiceTests
         string surname = "Doe";
         string password = "password";
         Role role = Role.User;
-        
+
         // Act
         _userService.AddUser(name, surname, password, role);
-        
+
         //Assert
         _userRepositoryMock.Verify(repo => repo.Add(name, surname, It.IsAny<string>(), role), Times.Once);
+    }
+
+    [Fact]
+    public void DeleteUser_ShouldDeleteUser()
+    {
+        //Arrange
+        int id = 1;
+
+        // Act
+        _userService.DeleteUser(id);
+
+        //Assert
+        _userRepositoryMock.Verify(repo => repo.Delete(id), Times.Once);
+    }
+
+    [Fact]
+    public void GetUserById_ShouldReturnUser()
+    {
+        //Arrange
+        int id = 1;
+        var expectedUser = new User("John", "Doe", "password", Role.User){Id = id};
+        _userRepositoryMock.Setup(repo => repo.GetById(id)).Returns(expectedUser);
+        
+        
+        
+        // Act
+        var result = _userService.GetUserById(id);
+        
+        //Assert
+        Assert.NotNull(result);
+        Assert.Equal(expectedUser, result);
+        _userRepositoryMock.Verify(repo => repo.GetById(id), Times.Once);
+    }
+    
+    [Fact]
+    public void GetUserById_ShouldReturnNull()
+    {
+        //Arrange
+        int id = 1;
+        _userRepositoryMock.Setup(repo => repo.GetById(id)).Returns((User?)null);
+        
+        // Act
+        var result = _userService.GetUserById(id);
+        
+        //Assert
+        Assert.Null(result);
+        _userRepositoryMock.Verify(repo => repo.GetById(id), Times.Once);
+    }
+    
+    // runs after every test
+    public void Dispose()
+    {
+        // checks if Log method was called after each test
         _loggerMock.Verify(logger => logger.Log(It.IsAny<string>()), Times.Once);
     }
 }
