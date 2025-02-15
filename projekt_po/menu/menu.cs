825321e7ddc291f170e2703c2aee92fa30e68c4d
Krzychu <krzysztof.springer@uczen.zsk.poznan.pl>
2025-02-15 21:34:21 +0100

    dodalem menu

---
+++ projekt_po/menu/menu.cs
@@ -0,0 +1,16 @@
+﻿ using projekt_po.Services;
+
+ public class UserMenu
+ {
+     private readonly UserService _userService;
+
+     public UserMenu(UserService userService)
+     {
+         _userService = userService;
+     }
+
+     public void Showmenu()
+     {
+         _userService.ListUsers();
+     }
+ }
