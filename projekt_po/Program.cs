29ae1e7e3b540be0b5b3c25f934251bf6ad7e908
pilif139 <pifilif@gmail.com>
2025-03-26 20:29:43 +0100

    you can run database seeder with 'dotnet run seed'

--- projekt_po/Program.cs
+++ projekt_po/Program.cs
@@ -9,41 +9,63 @@ using projekt_po.Utils;
 
 // uses DotNetEnv package to load secret variables from .env files
 Env.Load();
-// defines services collection and adds DatabaseContext to it so every service like user, auth etc. will use
-// the same database connection.
 
+// logger is a singleton service that will be used by other services like AuthService, UserRepository etc.
 ILogger logger = new Logger("log.txt");
 
+// defines services collection and adds DatabaseContext to it so every service like user, auth etc. will use
+// the same database connection.
 var services = new ServiceCollection();
-services.AddDbContext<DatabaseContext>(); // could be AddSingleton too
+services.AddDbContext<DatabaseContext>();
 services.AddSingleton(logger);
-services.AddSingleton<AuthService>();
+
+// services and repositories
+services.AddSingleton<IAuthService, AuthService>();
 services.AddSingleton<IRbacService, RbacService>();
 services.AddTransient<IUserRepository, UserRepository>();
 services.AddTransient<UserService>();
+
+// seeder
+services.AddSingleton<DatabaseSeeder>();
+
+//menus
+services.AddTransient<AuthMenu>();
+services.AddTransient<UserMenu>();
+services.AddTransient<AdminMenu>();
 // add more services here like this: services.AddTransient<MyService1>();
 
 var serviceProvider = services.BuildServiceProvider();
 
+//handling seeder
+var commandLineArgs = Environment.GetCommandLineArgs();
+if (commandLineArgs.Contains("seed"))
+{
+    var seeder = serviceProvider.GetService<DatabaseSeeder>()!;
+    seeder.Seed();
+    Console.WriteLine("Database seeded successfully");
+    return;
+}
+
 // get services like this: var service1 = serviceProvider.GetService<MyService1>(); where MyService1 is a class that you added to
 // services collection and in the class constructor you can inject other services like UserRepository, DatabaseContext etc.
-var userService = serviceProvider.GetService<UserService>();
-var authService = serviceProvider.GetService<AuthService>();
-if(authService == null || userService == null)
+var authMenu = serviceProvider.GetService<AuthMenu>();
+var userMenu = serviceProvider.GetService<UserMenu>();
+var adminMenu = serviceProvider.GetService<AdminMenu>();
+var authService = serviceProvider.GetService<IAuthService>();
+
+if (authMenu == null || userMenu == null || adminMenu == null || authService == null)
 {
     throw new Exception("Services not found");
 }
 
-// under there will be defined menus and their options
-
-var authMenu = new AuthMenu(authService);
-var userMenu = new UserMenu(userService);
-var adminMenu = new AdminMenu(userService);
 
 bool showMenu = true;
 while (showMenu)
 {
+    Console.Clear();
+    // auth menu is shown first and it returns true if user logs in successfully and false if user wants to exit the app
     showMenu = authMenu.ShowMenu();
+    //after logging in show menu based on user role
     var role = authService.GetLoggedUserRole();
     switch (role)
     {
@@ -53,8 +75,10 @@ while (showMenu)
         case Role.Client:
             userMenu.ShowUserMenu();
             break;
+        case Role.Worker:
+            // worker menu
+            break;
         default:
-            Console.WriteLine("Unknown role");
             return;
     }
 }
